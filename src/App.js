3682dfa3c27fb522cadf522b1f9bbdb706c79e3b
MadikMayCry <d1e.4.liberty@gmail.com>
2019-10-29 22:57:50 +0600

    Posts
    And other stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Posts from "./components/Posts";
+import About from "./components/About";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "antd/dist/antd.css";
+import {
+  Layout,
+  Menu,
+  Button,
+  Breadcrumb,
+  Icon,
+  Card,
+  Avatar,
+  Row,
+  Anchor,
+  Divider,
+  Descriptions,
+  Tag,
+  Tooltip
+} from "antd";
+
+const { SubMenu } = Menu;
+
+const { Header, Content, Footer, Sider } = Layout;
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Layout>
+          <Header className="header">
+            <div className="logo" />
+            <Menu theme="dark" mode="horizontal" style={{ lineHeight: "64px" }}>
+              <Menu.Item key="1">
+                <Link to="/">Home</Link>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Link to="/about">About</Link>
+              </Menu.Item>
+            </Menu>
+          </Header>
+          <Content style={{ padding: "0 50px" }}>
+            <Layout style={{ padding: "24px 0", background: "#fff" }}>
+              <Switch>
+                <Route exact path="/">
+                  <Posts />
+                </Route>
+                <Route path="/about">
+                  <About />
+                </Route>
+              </Switch>
+            </Layout>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Ant Design ©2018 Created by Ant UED
+          </Footer>
+        </Layout>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Posts/index.js
@@ -0,0 +1,137 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import { Link } from "react-router-dom";
+
+import "antd/dist/antd.css";
+import {
+  Layout,
+  Menu,
+  Button,
+  Breadcrumb,
+  Icon,
+  Card,
+  Avatar,
+  Row,
+  Anchor,
+  Col,
+  Divider,
+  Descriptions,
+  Tag,
+  Tooltip
+} from "antd";
+
+const { SubMenu } = Menu;
+
+const { Meta } = Card;
+const { Header, Content, Footer, Sider } = Layout;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      activeFaculty: "Business School",
+      facultyNames: {},
+      data: {},
+      currentData: {}
+    };
+  }
+
+  async componentDidMount() {
+    await this.getData();
+    await this.sortByFaculty();
+  }
+
+  changeFaculty = props => {
+    let tempFac = props.key;
+
+    this.setState({
+      activeFaculty: tempFac
+    });
+  };
+
+  posts = () =>
+    this.state.currentData.map((item, index) => (
+      <Col span={6} className="user-post-wrapper" key={index} id={item.id}>
+        <Card
+          style={{ margin: "10px" }}
+          className="user-post"
+          cover={<img alt={item.title} src={item.image} />}
+          actions={[
+            <Tooltip title="Response">
+              <Button type="link" shape="round" icon="plus">
+                Response
+              </Button>
+            </Tooltip>,
+            <Tooltip title="Details">
+              <Button type="link">
+                Details
+                <Icon type="right" />
+              </Button>
+            </Tooltip>
+          ]}
+        >
+          <Meta
+            title={<Tooltip title={item.title}>{item.title}</Tooltip>}
+            description={item.description}
+          />
+        </Card>
+      </Col>
+    ));
+
+  aside = () =>
+    this.state.facultyNames.map((item, index) => (
+      <Menu.Item key={item} onClick={this.changeFaculty}>
+        <Icon type="desktop" />
+        <span>{item}</span>
+      </Menu.Item>
+    ));
+
+  sortByFaculty = async () => {
+    let tempData = this.state.data.filter(
+      item => item.faculty == this.state.activeFaculty
+    );
+
+    let uniqType = Array.from(
+      new Set(this.state.data.map(e => e.faculty))
+    ).filter(e => e);
+
+    this.setState({
+      currentData: tempData,
+      facultyNames: uniqType,
+      loading: false
+    });
+  };
+
+  getData = async () => {
+    await axios.get(`data.json`).then(res =>
+      this.setState({
+        data: res.data
+      })
+    );
+  };
+
+  render() {
+    return (
+      <>
+        <Sider width={"auto"} style={{ background: "#fff" }}>
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={["1"]}
+            defaultOpenKeys={["sub1"]}
+            style={{ height: "100%" }}
+          >
+            {this.state.loading ? <div>Loading...</div> : this.aside()}
+          </Menu>
+        </Sider>
+        <Content style={{ padding: "0 24px", minHeight: 280 }}>
+          <Row>{this.state.loading ? <div>Loading...</div> : this.posts()}</Row>
+        </Content>
+      </>
+    );
+  }
+}
+
+export default App;
