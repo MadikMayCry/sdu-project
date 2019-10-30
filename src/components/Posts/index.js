387a3521c12d16b374d663575a51f226919cf1ef
MadikMayCry <d1e.4.liberty@gmail.com>
2019-10-30 14:46:11 +0600

    Listed posts

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -19,13 +19,34 @@ import {
   Divider,
   Descriptions,
   Tag,
-  Tooltip
+  Tooltip,
+  List
 } from "antd";
 
 const { SubMenu } = Menu;
 
 const { Meta } = Card;
 const { Header, Content, Footer, Sider } = Layout;
+
+const listData = [];
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: "/post",
+    title: `ant design part ${i}`,
+    avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
+    description:
+      "Ant Design, a design language for background applications, is refined by Ant UED Team.",
+    content:
+      "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently."
+  });
+}
+
+const IconText = ({ type, text }) => (
+  <span>
+    <Icon type={type} style={{ marginRight: 8 }} />
+    {text}
+  </span>
+);
 
 class App extends Component {
   constructor(props) {
@@ -56,6 +77,46 @@ class App extends Component {
     );
   };
 
+  listPosts = () => {
+    return (
+      <List
+        itemLayout="vertical"
+        size="large"
+        pagination={{
+          onChange: page => {
+            console.log(page);
+          },
+          pageSize: 5
+        }}
+        dataSource={this.state.currentData}
+        // dataSource={listData}
+        renderItem={item => (
+          <List.Item
+            key={item.title}
+            actions={[
+              <IconText type="star-o" text="156" key="list-vertical-star-o" />,
+              <IconText type="like-o" text="156" key="list-vertical-like-o" />,
+              <IconText type="message" text="2" key="list-vertical-message" />
+            ]}
+            extra={
+              <img
+                width={272}
+                alt="logo"
+                src={item.image}
+              />
+            }
+          >
+            <List.Item.Meta
+              avatar={<Avatar src={item.avatar} />}
+              title={<a href={item.href}>{item.title}</a>}
+              description={item.description}
+            />
+            {item.content}
+          </List.Item>
+        )}
+      />
+    );
+  };
   posts = () =>
     this.state.currentData.map((item, index) => (
       <Col span={8} className="user-post-wrapper" key={index} id={item.id}>
@@ -131,7 +192,9 @@ class App extends Component {
           </Menu>
         </Sider>
         <Content style={{ padding: "0 24px", minHeight: 280 }}>
-          <Row>{this.state.loading ? <div>Loading...</div> : this.posts()}</Row>
+          <Row>
+            {this.state.loading ? <div>Loading...</div> : this.listPosts()}
+          </Row>
         </Content>
       </>
     );
